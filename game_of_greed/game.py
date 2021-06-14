73195ae810c87e20755cecef3a51d07676263f2c
MohmmadNada <mohammad.nada_97@yahoo.com>
2021-06-14 18:27:29 +0300

    more reada\ble

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -1,10 +1,10 @@
 from game_of_greed.game_logic import GameLogic,Banker
 
 class Game:
-    def __init__(self, roller=None,score=0):
+    def __init__(self, roller=None):
         self.roller = roller
-        self.calculate_score=score
-
+        self.total=0
+        self.round=1
     def play(self):
         print("Welcome to Game of Greed")
         user_input = input("Wanna play? ")
@@ -13,9 +13,8 @@ class Game:
         else:
             int_banker=Banker()
             counter_remaining=6
-            counter_looping=1
             while(True):
-                print(f'Starting round {counter_looping}')
+                print(f'Starting round {self.round}')
                 print('Rolling 6 dice...')
                 dice = self.roller(6)
                 printable_dice = ','.join([str(d) for d in dice])
@@ -37,23 +36,15 @@ class Game:
                     int_banker.shelf(score)
                     ask_for_roll_again=input('(r)oll again, (b)ank your points or (q)uit ')
                     int_banker.bank()
+                    self.total+=int_banker.balance
+                    new_total=self.total
                     if ask_for_roll_again=='b':
                         print(f'You banked {int_banker.balance} points in round 1')
-                        print(f'Total score is {int_banker.balance} points')
-                        counter_looping+=1
-
-    
-
-
+                        print(f'Total score is {new_total} points')
+                        self.round+=1
 
 
 if __name__ == "__main__":
     roller = GameLogic.roll_dice
-    calc_score=GameLogic.calculate_score
-    int_banker=Banker()
-    # shelf=int_banker.shelf()
-    # bank=int_banker.bank()
-    # int_banker.shelf(100)
-    # print(int_banker.shelved)
-    game = Game(roller,calc_score)
+    game = Game(roller)
     game.play()
